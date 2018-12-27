f71d43c806bef789059c2aae220d44fa0100348e
xiayefeng <xiayu_12@yeah.net>
2018-12-27 19:08:02 +0800

    detail optimize

--- config.js
+++ config.js
@@ -11,29 +11,19 @@ function request(url, data = {}, method = "GET") {
       url: url,
       data: data,
       method: method,
-      header: method == 'GET' ? {
-        'Content-Type': 'application/json'
-      } : {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
+      header: {'Content-Type': method === 'GET' ? 'application/json' : 'application/x-www-form-urlencoded'},
       success: function (res) {
         if (res.statusCode == 200) {
-            resolve(res.data)
-          /* if (res.data) {
-            if (res.data.code == 0 || res.data.code == 1000) {
+          resolve(res.data)
+          /*if (res.data) {
+            if (res.data.code == 0) {
               resolve(res.data);
-            } else if (res.data.code == 1013) {
-              wx.showModal({
-                title: '提示',
-                content: res.data.msg,
-                showCancel: false
-              })
             } else {
               throwError(res.data, reject)
             }
           } else {
-            throwError(res.data, reject)
-          } */
+            throwError(res, reject)
+          }*/
         } else {
           reject({
             message: '网络错误，请稍后重试',
@@ -42,7 +32,7 @@ function request(url, data = {}, method = "GET") {
         }
       },
       fail: function (err) {
-        if(err.errMsg && ! err.message){
+        if (err.errMsg && !err.message) {
           err.message = err.errMsg
         }
         reject(err)
